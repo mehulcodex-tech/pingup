ec39c3aa2e3452b3375e80b0fc1144c93a3c0038
Mehul Gupta <guptamehul051@gmail.com>
2026-07-09 20:14:32 +0530

    Setup MongoDB and user model

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import connectDB from "./configs/db.js";
 
 const app = express();
 
+await connectDB(); 
+
 app.use(express.json());
 app.use(cors());
-
+ 
 app.get("/", (req, res) => res.send("Server is running"));
 
 const PORT = process.env.PORT || 4000;
