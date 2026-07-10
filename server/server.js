9600f748b0d3ec5d8846e80173d54ecce7fd0007
Mehul Gupta <guptamehul051@gmail.com>
2026-07-10 21:50:22 +0530

    inngest

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./configs/db.js";
+import {serve} from "inngest/express";
+import {inngest, functions} from "./inngest/index.js"
 
 const app = express();
 
@@ -11,6 +13,7 @@ app.use(express.json());
 app.use(cors());
  
 app.get("/", (req, res) => res.send("Server is running"));
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 const PORT = process.env.PORT || 4000;
 
